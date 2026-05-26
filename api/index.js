b58cb77f61759f49ebfe065468e13e985adf4504
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-25 21:05:10 -0300

    chore: update shareCode to WKMQVWZNFTZ

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=3E888FJ8A0U&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=WKMQVWZNFTZ&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
