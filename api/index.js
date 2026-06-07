56a903b2951412871b52b4f36de98c697a869529
JCPrime01 <vanzokiller@gmail.com>
2026-06-07 12:33:20 -0300

    chore: shareCode MR7DWOPHZOY -> TL2YVZSOT3M (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=MR7DWOPHZOY&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=TL2YVZSOT3M&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
