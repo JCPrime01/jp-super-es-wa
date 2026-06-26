00126832482b26297a24eb76fb5b9751be58f6f4
JCPrime01 <vanzokiller@gmail.com>
2026-06-26 09:28:21 -0300

    chore: shareCode MTIWTE4TCAB -> ZM02W2CK4TI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=MTIWTE4TCAB&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=ZM02W2CK4TI&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
