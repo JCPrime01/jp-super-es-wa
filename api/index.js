3201c6df2882ace9658dd9b1a07530f7ef44be28
JCPrime01 <vanzokiller@gmail.com>
2026-06-30 09:17:56 -0300

    chore: shareCode 59FBKJX77EO -> NEW7XPX51US (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=59FBKJX77EO&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=NEW7XPX51US&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
