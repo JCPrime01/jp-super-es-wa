3436ccc606d08573d7482333c3a1c6970530c009
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 21:49:29 -0300

    chore: shareCode EXTK8J13VVF -> BRY4QXX9IQE (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=EXTK8J13VVF&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=BRY4QXX9IQE&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
