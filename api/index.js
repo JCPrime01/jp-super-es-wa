b0b1941d9f5cca724a1b01b0037cf7b5631c4779
JCPrime01 <vanzokiller@gmail.com>
2026-07-08 09:31:29 -0300

    chore: shareCode 929784ZBLYF -> KRTCKCZ3HXR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=929784ZBLYF&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=KRTCKCZ3HXR&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
