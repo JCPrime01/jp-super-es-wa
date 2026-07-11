faa348d9d9d072dc3dbbc7dd3db2c9dec4559221
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 08:39:54 -0300

    chore: shareCode N1OCPIGQ2GV -> 391SZLEFUP2 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=N1OCPIGQ2GV&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=391SZLEFUP2&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
