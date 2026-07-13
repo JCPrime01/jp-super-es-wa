a5ec6331d328abb983b2e7f38c7391f2dd0dd263
JCPrime01 <vanzokiller@gmail.com>
2026-07-13 10:52:36 -0300

    chore: shareCode 5G11APC7J93 -> 3AGPONWCZJK (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=5G11APC7J93&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=3AGPONWCZJK&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
