5854057946b6cedbb18e86adf83a91faf413265c
JCPrime01 <vanzokiller@gmail.com>
2026-07-20 10:14:35 -0300

    chore: shareCode YDGOOQ10UZZ -> 5Y5J5CIOOGU (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=YDGOOQ10UZZ&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=5Y5J5CIOOGU&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
