41e5048acf264e995c7b8b01d7d48085240731f4
JCPrime01 <vanzokiller@gmail.com>
2026-08-04 11:58:19 -0300

    chore: shareCode SR800IGK9AG -> H5T96ADFXKF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=SR800IGK9AG&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=H5T96ADFXKF&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
