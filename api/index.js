ef0a51e68965930c7f94e00a656a34915d08ba8f
JCPrime01 <vanzokiller@gmail.com>
2026-08-06 11:41:47 -0300

    chore: shareCode H5T96ADFXKF -> 8BCFMOKQC6H (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=H5T96ADFXKF&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=8BCFMOKQC6H&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
