55b2851cd05010550c66e29a7cfeb8e1c29de5aa
JCPrime01 <vanzokiller@gmail.com>
2026-08-10 10:38:32 -0300

    chore: shareCode Y5DFR810ANO -> Q2FDT8AEXA1 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=Y5DFR810ANO&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=Q2FDT8AEXA1&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
